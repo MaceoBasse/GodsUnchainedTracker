2fae0e2a9cdd3096d67dfdd44c2bc701d9dfadec
Maceo Basse <basse.maceo@gmail.com>
2021-12-28 02:24:34 +0100

    Add save

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import time
+import pickle
+
 
 def card_tracker(cards_name, target):
     url = "https://api.x.immutable.com/v1/orders?direction=asc&include_fees=true&order_by=buy_quantity&page_size=1&sell_token_address=0xacb3c6a43d15b907e8433077b6d38ae40936fe2c&sell_token_type=ERC721&status=active&sell_token_name=" + cards_name + "&buy_token_type=ETH"
@@ -13,14 +15,20 @@ def card_tracker(cards_name, target):
     price = (quantity / pow(10, decimal))
 
     print("Le prix de la carte", cards_name, "est de",
-            price, "eth et la target est de", target)
+          price, "eth et la target est de", target)
 
     if price <= target:
         print("Achat")
 
 
 list_cards = []
+save = input("Veux-tu utiliser ta sauvegarde ? (Oui,Non)")
 while True:
+    if save == "Oui":
+        with open("list_cards.txt", "rb") as fp:   # Unpickling
+            list_cards = pickle.load(fp)
+            print(list_cards)
+            break
     card = []
     cards_name = input("Nom de la carte ? :  ")
     target = float(input("Prix souhaiter  ? :  "))
@@ -29,10 +37,16 @@ while True:
     list_cards.append(card)
     next = input("Voulez-vous ajouter une carte (Enter ou Non)")
     if next == "Non":
+        save = input("Veux-tu sauvegarder la liste de cartes ? (Oui,Non)")
+        if save == "Oui":
+            with open("list_cards.txt", "wb") as fp:  # save
+                pickle.dump(list_cards, fp)
+                print("sauvegarde")
         break
 
-for i in range(len(list_cards)):
-    cards_name = list_cards[i][0]
-    target = list_cards[i][1]
-    card_tracker(cards_name, target)
-    time.sleep(300)  # wait 5 min
+while True:
+    for i in range(len(list_cards)):
+        cards_name = list_cards[i][0]
+        target = list_cards[i][1]
+        card_tracker(cards_name, target)
+        time.sleep(300)  # wait 5 min
