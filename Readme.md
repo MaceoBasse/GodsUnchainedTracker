d0e4a1c0be94adbb3c4ac5cad34bd6a50eefb4d1
Maceo Basse <basse.maceo@gmail.com>
2021-12-28 09:38:37 +0100

    Add quantity and notification

--- Readme.md
+++ Readme.md
@@ -3,3 +3,6 @@
 ### git clone https://github.com/MaceoBasse/GodsUnchainedTracker.git
 
 ### pip3 install requests
+### pip3 install pushbullet.py
+
+### modify Pushbullet api_key
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 import time
 import pickle
+from pushbullet import Pushbullet
 
+pb = Pushbullet(api_key)
 
-def card_tracker(cards_name, target):
-    url = "https://api.x.immutable.com/v1/orders?direction=asc&include_fees=true&order_by=buy_quantity&page_size=1&sell_token_address=0xacb3c6a43d15b907e8433077b6d38ae40936fe2c&sell_token_type=ERC721&status=active&sell_token_name=" + cards_name + "&buy_token_type=ETH"
+#regler qualite
+def card_tracker(cards_name, target, quality):
+    url = 'https://api.x.immutable.com/v1/orders?direction=asc&include_fees=true&order_by=buy_quantity&page_size=1&sell_token_address=0xacb3c6a43d15b907e8433077b6d38ae40936fe2c&sell_token_type=ERC721&status=active&sell_token_name="%s"&sell_metadata={"quality":["%s"]}&buy_token_type=ETH'%(cards_name, quality)
     response = requests.get(url)
     data = response.json()
 
@@ -18,7 +21,11 @@ def card_tracker(cards_name, target):
           price, "eth et la target est de", target)
 
     if price <= target:
-        print("Achat")
+        print("Il faut acheter la carte", cards_name)
+        if notif == 'Oui':
+            title = "Il faut acheter la carte" + cards_name
+            body = "Le prix de la carte" + cards_name + "est de" + price + "eth et la target est de" + target
+            push = pb.push_note(title, body)
 
 
 list_cards = []
@@ -31,7 +38,8 @@ while True:
             break
     card = []
     cards_name = input("Nom de la carte ? :  ")
-    target = float(input("Prix souhaiter  ? :  "))
+    quality = input("Qualité de la carte ? (Meteorite,Shadow,Gold,Diamond) :  ")
+    target = float(input("Seuil de vente  ? :  "))
     card.append(cards_name)
     card.append(target)
     list_cards.append(card)
@@ -42,11 +50,13 @@ while True:
             with open("list_cards.txt", "wb") as fp:  # save
                 pickle.dump(list_cards, fp)
                 print("sauvegarde")
+        notif = input("Veux-tu les notifications ? (Oui, Non) :")
         break
 
 while True:
     for i in range(len(list_cards)):
         cards_name = list_cards[i][0]
         target = list_cards[i][1]
-        card_tracker(cards_name, target)
+        quality = list_cards[i][2]
+        card_tracker(cards_name, target,quality)
         time.sleep(300)  # wait 5 min
